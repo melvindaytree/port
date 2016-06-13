aacab9836f9f5ce860456671f6f12af9c0167277
Melvin Daytree <melvin.daytree@uconn.edu>
2016-06-13 11:13:19 -0400

    pics done

---
+++ portfolio/js/main.js
@@ -0,0 +1,26 @@
+
+$(".project1").hover(function () {
+    $(".project1").find(".projecthover").stop(true).slideToggle("slow");
+});
+
+$(".project2").hover(function () {
+    $(".project2").find(".projecthover").stop(true).slideToggle("slow");
+});
+
+$(".project3").hover(function () {
+    $(".project3").find(".projecthover").stop(true).slideToggle("slow");
+});
+
+$(".project4").hover(function () {
+    $(".project4").find(".projecthover").stop(true).slideToggle("slow");
+});
+
+$(".project5").hover(function () {
+    $(".project5").find(".projecthover").stop(true).slideToggle("slow");
+});
+
+$(".project6").hover(function () {
+    $(".project6").find(".projecthover").stop(true).slideToggle("slow");
+});
+
+
