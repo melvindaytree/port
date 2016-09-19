f377e347d74b29c0f566bed68ec34b1a086271c5
Melvin Daytree <melvin.daytree@uconn.edu>
2016-09-18 21:17:29 -0400

    updated skills page and started fixing the website part

--- portfolio/contact.php
+++ portfolio/contact.php
@@ -1,26 +1,35 @@
 <?php
 
-$message = '<html><body>';
-$message .= "Requestor: " . "<strong>" . $_POST['name'] . "</strong>" . "<br /><br />" ."\r\n"; 
-$message .= "Email Address: " . "<strong>" . $_POST['email'] . "</strong>" . "<br /><br />" . "\r\n";
-$message .= "Comments: " . "<strong>" . $_POST['comments'] . "</strong>" . "<br /><br />" . "\r\n";
-$message .= '<html><body>' ?>
+$name = $_POST['name'];
+$email = $_POST['email'];
+$comments = $_POST['comments'];
 
 
-<?php
+    if (empty($_POST['name']))
+        $errors['name'] = 'Name is required.';
 
-$mailto = 'mdaytree@gmail.com';
-$subject = 'Request';
-$from = $_POST['email'];
-$headers .= "MIME-Version: 1.0\r\n";
-$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+    if (empty($_POST['email']))
+        $errors['email'] = 'Email is required.';
 
-// Send
-	 mail($mailto, 'Request', $message, $headers);
- if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-  $emailErr = "Invalid email format"; 
-       }
+    if (empty($_POST['comments']))
+        $errors['comments'] = 'Comments are required';
 
+        // DO ALL YOUR FORM PROCESSING HERE
+
+        //Send an email 
+        $message = '<html><body>';
+        $message .= "Requestor: " . "<strong>" . $_POST['name'] . "</strong>" . "<br /><br />" ."\r\n"; 
+        $message .= "Email Address: " . "<strong>" . $_POST['email'] . "</strong>" . "<br /><br />" . "\r\n";
+        $message .= "Comments: " . "<strong>" . $_POST['comments'] . "</strong>" . "<br /><br />" . "\r\n";
+        $message .= '<html><body>';
+
+
+        $mailto = 'mdaytree@gmail.com';
+        $subject = 'Request';
+        $from = $_POST['email'];
+        $headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+        // Send
+            mail($mailto, $subject, $message, $headers);
+            echo "success";
 ?>
-
-<h1 class="text-center">Thanks for contacting me you will hear back as soon as possible!</h1>
--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -62,3 +62,59 @@ $("#language").toggle( function () {
         $("#program").addClass("program");
         $("#language").delay( 3000 ).toggle();
     });
+
+/* Slide down effect for the programs 
+$( "#h4languages" ).click(function() {
+    console.log("slide");
+    $("#languages").slideDown( "slow" )});
+
+$( "#h4applications" ).click(function() {
+    console.log("slide");
+    $("#applications").slideDown( "slow" )});
+*/
+
+//For form submission using Ajax
+$(document).ready(function() {
+
+    // process the form
+    $('#submit').click(function(event) {
+
+        console.log("step1");
+        $('#progress').width('20%');
+
+        // get the form data
+        // there are many ways to get this data using jQuery (you can use the class or id also)
+        var formData = {
+            'name'              : $('input[name=name]').val(),
+            'email'             : $('input[name=email]').val(),
+            'comments'    :     $('textarea[name=comments]').val()
+        };
+    console.log("step2");
+    $('#progress').width('55%');
+        // process the form
+        $.ajax({
+            type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+            url         : '/contact.php', // the url where we want to POST
+            data        : formData, // our data object
+            dataType    : 'json', // what type of data do we expect back from the server
+                        encode          : true
+        })
+            // using the done promise callback
+            .done(function(data) {
+
+                // log data to the console so we can see
+                console.log(data); 
+
+                // here we will handle errors and validation messages
+            });
+console.log("step3");
+$('#progress').width('100%');
+        // stop the form from submitting the normal way and refreshing the page
+        event.preventDefault();
+        $('#name').val("");
+        $('#email').val("");
+        $('#comments').val("");
+        setTimeout( function() {$('#progress').width(0)}, 3000);
+    });
+        
+});
