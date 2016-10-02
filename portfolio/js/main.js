b2980c12a89497ff0d5ca92d15dd4fcd3d0e33a0
Melvin Daytree <melvin.daytree@uconn.edu>
2016-10-02 19:03:58 -0400

    redesigning the actual portfolio section

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,4 +1,18 @@
 
+function toggleDisplay(id) {
+    $(id).click(function() {
+    $(id).find(".projecthover").stop(true).toggle("slow");
+    });
+};
+
+toggleDisplay(".project1");
+toggleDisplay(".project2");
+toggleDisplay(".project3");
+toggleDisplay(".project4");
+toggleDisplay(".project5");
+toggleDisplay(".project6");
+
+/*
 $(".project1").click(function () {
     $(".project1").find(".projecthover").stop(true).toggle("slow");
 });
@@ -14,54 +28,8 @@ $(".project3").click(function () {
 $(".project4").click(function () {
     $(".project4").find(".projecthover").stop(true).toggle("slow");
 });
+*/
 
-$(".project5").click(function () {
-    $(".project5").find(".projecthover").stop(true).toggle("slow");
-});
-
-$(".project6").hover(function () {
-    $(".project6").find(".projecthover").stop(true).slideToggle("slow");
-});
-
-$(".project7").hover(function () {
-    $(".project7").find(".projecthover").stop(true).slideToggle("slow");
-});
-
-$(".project8").hover(function () {
-    $(".project8").find(".projecthover").stop(true).slideToggle("slow");
-});
-
-$(".project9").hover(function () {
-    $(".project9").find(".projecthover").stop(true).slideToggle("slow");
-});
-
-$("#language").toggle( function () {
-        $("#language").addClass("expand");
-        $(".center-box").css("width", "1400px");
-        $("#language").removeClass("language");
-        $("#program").toggle();
-    },
-    
-    function () {
-        $("#language").removeClass("expand");
-        $(".center-box").css( "width", "660px" );
-        $("#language").addClass("language");
-        $("#program").delay( 3000 ).toggle();
-    });
-
-    $("#program").toggle( function () {
-        $("#program").addClass("expand");
-        $(".center-box").css("width", "1400px");
-        $("#program").removeClass("program");
-        $("#language").toggle();
-    },
-    
-    function () {
-        $("#program").removeClass("expand");
-        $(".center-box").css( "width", "660px" );
-        $("#program").addClass("program");
-        $("#language").delay( 3000 ).toggle();
-    });
 
 /* Slide down effect for the programs 
 $( "#h4languages" ).click(function() {
@@ -117,4 +85,23 @@ $('#progress').width('100%');
         setTimeout( function() {$('#progress').width(0)}, 2000);
     });
         
+});
+
+//Check if form is filled out
+(function() {
+    $('form > input').keyup(function() {
+        console.log("blue");
+        var empty = false;
+        $('form > input').each(function() {
+            if ($(this).val() == '') {
+                empty = true;
+            }
+        });
+
+        if (empty) {
+            $('#submit').attr('disabled', 'disabled'); // updated according to http://stackoverflow.com/questions/7637790/how-to-remove-disabled-attribute-with-jquery-ie
+        } else {
+            $('#submit').removeAttr('disabled'); // updated according to http://stackoverflow.com/questions/7637790/how-to-remove-disabled-attribute-with-jquery-ie
+        }
+    });
 });
